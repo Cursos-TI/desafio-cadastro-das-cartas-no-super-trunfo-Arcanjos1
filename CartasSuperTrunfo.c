a15b430cca3409e1013fbfe749f3ac1f70f4a262
Arcanjos1 <arcanjoii@live.com>
2025-07-22 00:37:58 +0000

    Nivel Aventureiro Finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -15,6 +15,10 @@ int populaçao_1;
 float área_1;
 float PIB_1;
 int turistas_1;
+// Adiçao das varivaeis do nivel aventureiro
+float densidade_1;
+float PIB_cap1;
+
 
 // Carta 2
 char estado_2;
@@ -24,6 +28,10 @@ int populaçao_2;
 float área_2;
 float PIB_2;
 int turistas_2;
+// Adiçao das variaveis do nivel aventureiro
+float densidade_2;
+float PIB_cap2;
+
 
 // Entrada de dados carta 1
 printf("Cadastro da carta 1\n");
@@ -74,26 +82,39 @@ printf("Numero de pontos turísticos: \n");
 scanf("%d", &turistas_2);
 printf("\n");
 
+//calculo da densidade populacional carta 1 e 2
+densidade_1 = (float)populaçao_1/área_1;
+densidade_2 = (float)populaçao_2/área_2;
+
+// Calculo do pib per capita
+PIB_cap1 = (float)PIB_1/populaçao_1;
+PIB_cap2 = (float)PIB_2/populaçao_2;
+
 //Saída de dados carta 1
 printf("Carta 1 \n");
 printf("Estado: %c\n", estado_1);
 printf("Código: %s\n", codigo_carta_1);
 printf("Nomeda cidade: %s\n", nome_cidade_1);
 printf("Populaçao: %d\n", populaçao_1);
-printf("Área: %f km²\n", área_1);
-printf("PIB: %f Bilhoes de reais\n", PIB_1);
+printf("Área: %.2f km²\n", área_1);
+printf("PIB: %.2f Bilhoes de reais\n", PIB_1);
 printf("Numero de pontos turisticos: %d\n", turistas_1);
+printf("Desnidade Populacional: %.2f  hab/km²\n", densidade_1);
+printf("PIB per Capita: %2.f reais\n", PIB_cap1);
 
 //Saída de dados carta 2
-
 printf("Carta 2 \n");
 printf("Estado: %c\n", estado_2);
 printf("Código: %s\n", codigo_carta_2);
-printf("Nomeda cidade: %s\n", nome_cidade_2);
+printf("Nome da cidade: %s\n", nome_cidade_2);
 printf("Populaçao: %d\n", populaçao_2);
-printf("Área: %f km²\n", área_2);
-printf("PIB: %f Bilhoes de reais\n", PIB_2);
+printf("Área: %.2f km²\n", área_2);
+printf("PIB: %.2f Bilhoes de reais\n", PIB_2);
 printf("Numero de pontos turisticos: %d\n", turistas_2);
+printf("Desnidade Populacional: %.2f  hab/km²\n", densidade_2);
+printf("PIB per Capita: %2.f reais\n", PIB_cap2);
+
+
 
 
 
