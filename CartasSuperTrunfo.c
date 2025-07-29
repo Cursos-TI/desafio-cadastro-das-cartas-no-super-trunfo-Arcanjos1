3ff07450a078c2787d8a64c1292549a9506c26b7
Arcanjos1 <arcanjoii@live.com>
2025-07-29 01:42:39 +0000

    Nivel mestre!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,7 +5,7 @@ int main() {
 char estado_1;
 char codigo_carta_1[4];
 char nome_cidade_1[40];
-int populacao_1;
+unsigned long int populacao_1;
 float area_1;
 float PIB_1;
 int turistas_1;
@@ -18,7 +18,7 @@ float super1;
 char estado_2;
 char codigo_carta_2[4];
 char nome_cidade_2[40];
-int populacao_2;
+unsigned long int populacao_2;
 float area_2;
 float PIB_2;
 int turistas_2;
@@ -39,7 +39,7 @@ printf("Nome da cidade (exemplo: Minas_Gerais): \n");
 scanf("%s", nome_cidade_1);
 
 printf("Populaçao (sem pontos): \n");
-scanf(" %d", &populacao_1);
+scanf(" %lu", &populacao_1);
 
 printf("Área: \n");
 scanf("%f", &area_1);
@@ -64,7 +64,7 @@ printf("Nome da cidade (exempo: São_Paulo): \n");
 scanf("%s", nome_cidade_2);
 
 printf("Populaçao (sem pontos): \n");
-scanf("%d", &populacao_2);
+scanf("%lu", &populacao_2);
 
 printf("Área: \n");
 scanf(" %f", &area_2);
@@ -79,10 +79,10 @@ printf("\n");
 //calculo carta 1 e 2
 densidade_1 = (float)populacao_1/area_1;
 densidade_2 = (float)populacao_2/area_2;
-super1 = (float)populacao_1 + area_1 + PIB_1 + (float)turistas_1 +PIB_cap1 + (1.0 / densidade_1);
-super2 = (float)populacao_2 + area_2 + PIB_2 + (float)turistas_2 +PIB_cap2 + (1.0 / densidade_2);
 PIB_cap1 = (float)PIB_1/populacao_1;
 PIB_cap2 = (float)PIB_2/populacao_2;
+super1 = (float)populacao_1 + area_1 + PIB_1 + (float)turistas_1 +PIB_cap1 + (1.0 / densidade_1);
+super2 = (float)populacao_2 + area_2 + PIB_2 + (float)turistas_2 +PIB_cap2 + (1.0 / densidade_2);
 
 
 //Saída de dados carta 1
@@ -90,7 +90,7 @@ printf("Carta 1 \n");
 printf("Estado: %c\n", estado_1);
 printf("Código: %s\n", codigo_carta_1);
 printf("Nome da cidade: %s\n", nome_cidade_1);
-printf("Populacao: %d\n", populacao_1);
+printf("Populacao: %lu\n", populacao_1);
 printf("Área: %.2f km²\n", area_1);
 printf("PIB: %.2f Bilhoes de reais\n", PIB_1);
 printf("Numero de pontos turisticos: %d\n", turistas_1);
@@ -103,7 +103,7 @@ printf("Carta 2 \n");
 printf("Estado: %c\n", estado_2);
 printf("Codigo: %s\n", codigo_carta_2);
 printf("Nome da cidade: %s\n", nome_cidade_2);
-printf("Populacao: %d\n", populacao_2);
+printf("Populacao: %lu\n", populacao_2);
 printf("Area: %.2f km²\n", area_2);
 printf("PIB: %.2f Bilhoes de reais\n", PIB_2);
 printf("Numero de pontos turisticos: %d\n", turistas_2);
